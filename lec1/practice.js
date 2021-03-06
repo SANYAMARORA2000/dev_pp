79f7625ea7b3389097f74df19c68f8d4fc8d95bb
sanyam <sanyamarora2000@gmail.com>
2021-03-07 01:56:05 +0530

    practice 1

---
+++ lec1/practice.js
@@ -0,0 +1,74 @@
+console.log("class practice");
+
+//ways to intialise variables
+let a=10;
+console.log(a);
+
+let b=false;
+console.log(b);
+
+let c="abc";
+console.log(c);
+
+let e;
+console.log(e);
+
+if(true)
+{
+    let f="this is limited to this block only";
+    console.log(f);
+
+}
+//console.log(f);
+
+const g= 10;
+//g=20;
+console.log(g);
+
+let movies={};// this is an example of empty object
+
+let data={
+
+
+    name:"sanyam",
+    place: "pitampura"
+};
+let key="name";
+console.log(data[key]);
+console.log(data.name);
+console.log(data.place);
+
+console.log(10=="10");
+console.log(10==="10");
+
+let values=[
+    10,
+    false,
+    "abc",
+    {
+        name: "Steve Rogers",
+        place: "Queens",
+      },
+    [1,2,3,4,5,6]
+
+
+];
+console.log(values[3].name);
+
+
+// for(let key in data)
+// {
+//     console.log(key);
+// }
+
+
+
+
+
+//console.log()
+
+
+
+
+
+
