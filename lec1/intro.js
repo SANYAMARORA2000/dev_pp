fc67def18be208ae3dc5cf1a1e9e2e67d7f46902
sanyam <sanyamarora2000@gmail.com>
2021-03-06 19:17:34 +0530

    this is my fourth commit

--- lec1/intro.js
+++ lec1/intro.js
@@ -6,4 +6,21 @@ let b=20;
 console.log(a);
 console.log(b);
 
+// variable is not assigned a value
+let e; // takes up undefined
+console.log(e);
 
+if (true) {
+    let f = "I am inside if block !!!";
+    console.log(f);
+    // console.log(a);
+    // a = 20;
+  }
+  //console.log(f);
+
+  const pi = 3.14; // define
+//pi = 20; // resassignment is not allowed in const
+//console.log(pi);
+
+console.log(10=="10");//yeh yaha pe sirf value dekh rhaa hai
+console.log(10==="10");//yaha pe vo data type bhi dekhega
