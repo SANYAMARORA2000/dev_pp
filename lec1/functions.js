6bc7b4565a5e7af59f6a5160eb49458dad048e92
sanyam <sanyamarora2000@gmail.com>
2021-03-06 20:07:25 +0530

    this is my sixth commit

---
+++ lec1/functions.js
@@ -0,0 +1,18 @@
+//function body
+
+function sayhi(name)
+{
+    console.log(" is my name");
+    //return 10;
+
+}
+
+//function call
+//sayhi("sanyam");
+
+// let value = sayhi("Steve");
+// console.log(value);
+
+let value = sayhi();
+console.log( value );
+// console.log(sayHi);
