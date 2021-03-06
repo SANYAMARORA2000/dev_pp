9e982f5ca94c27544919c10082de3dfccfab16c2
sanyam <sanyamarora2000@gmail.com>
2021-03-07 02:41:07 +0530

    function practice

--- lec1/funcprac.js
+++ lec1/funcprac.js
@@ -1,12 +1,30 @@
 
 
-function hello(name)
-{
-    console.log(name+" is printintg this in function");
-    return 10;
-}
+// function hello(name)
+// {
+//     console.log(name+" is printintg this in function");
+//     return 10;
+// }
 
 
 
-console.log(hello("sanyam"));
+// console.log(hello("sanyam"));
 //console.log(hello());//this will give error as the function is not returning anything
+
+function getfirstname(fullname)
+{
+    let a=fullname.split(" ");
+    return a[0];
+}
+function getsecondname(fullname)
+{
+    let b=fullname.split(" ");
+    return b[1];
+}
+function sayhi(fullname,fun)
+{
+    let name=fun(fullname);
+    console.log(name);
+}
+sayhi("tony stark",getfirstname);
+sayhi("steve rogers",getsecondname);
