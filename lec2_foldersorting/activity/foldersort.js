3697b7d7f2d394ccf29d40417bb2fb73d64f2791
sanyam <sanyamarora2000@gmail.com>
2021-03-11 23:37:02 +0530

    abc

--- lec2_foldersorting/activity/foldersort.js
+++ lec2_foldersorting/activity/foldersort.js
@@ -3,7 +3,7 @@ const path = require("path");
 let extensions = require("./util");
 
 let folderpath="../../../PRACTICE_1"; 
-let extfolderpath;
+let extfolderpath;;
 
 function checkfolder(extension)
 {
