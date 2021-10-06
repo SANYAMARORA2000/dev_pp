1cc97493f6aac7bbe6b60548f58aacc202cf105e
sanyam <sanyamarora2000@gmail.com>
2021-10-07 00:12:26 +0530

    update

--- practice-react/todo/src/App.js
+++ practice-react/todo/src/App.js
@@ -1,11 +1,13 @@
 import Header from "./Components/Header";
 import Heading from "./Components/Heading";
 
+
 function App() {
   return (
     <div className="App">
       <Heading></Heading>
      <Header></Header>
+     
     </div>
   );
 }
--- practice-react/todo/src/Components/Header.jsx
+++ practice-react/todo/src/Components/Header.jsx
@@ -1,8 +1,9 @@
 
 import React, { useEffect, useState} from 'react';
 import Modal from 'react-modal';
+import './Header.css';
 
-
+Modal.setAppElement('#root');
 const getlocalitems=()=>{
     let lists=localStorage.getItem('lists');
     console.log(lists);
@@ -25,12 +26,34 @@ const Header = () => {
     const[customername,setcustomername]=useState()
     const[obj,setobj]=useState({});
     const[arr,setarr]=useState(getlocalitems());
+    const[modalisopen,setmodalisopen]=useState(false);
+    const[modalisopen1,setmodalisopen1]=useState(false);
+    const[searchterm,setsearchterm]=useState("")
    
 
     useEffect(()=>{
       localStorage.setItem('lists',JSON.stringify(arr));
     },[arr])
    
+      
+    const handledelete=(index)=>
+    {
+       var newlist=[...arr];
+       newlist.splice(index,1);
+       setarr(newlist);
+    }
+
+    const handlecheckout=()=>
+    {
+        setmodalisopen1(true)
+        console.log("hello");
+       
+        <Modal isOpen={modalisopen1}>
+            <h1>hello</h1>
+        </Modal>
+      
+        
+    }
     
 
     const handletask=()=>{
@@ -52,8 +75,29 @@ const Header = () => {
 
        
     return ( <div class="task-container">
+           <input type="text" placeholder="Search" onChange={(e)=>{setsearchterm(e.target.value)}} />
            
+           <button onClick={()=>{setmodalisopen(true)}}>checkin</button>
+           <Modal isOpen={modalisopen} onRequestClose={()=>setmodalisopen(false)}>
            <div class="task-input">
+
+                    <input type="text" placeholder="vehicleNumber" value={vehiclenum}  onChange={(e)=>{setvehiclenum(e.target.value)
+                    }}/>
+                    <input type="text" placeholder="vehicleType" value={vehicletype}  onChange={(e)=>{setvehicletype(e.target.value)
+                    }}/>
+                    <input type="text" placeholder="vehicleModel" value={vehiclemodel}  onChange={(e)=>{setvehiclemodel(e.target.value)
+                    }}/>
+                    <input type="text" placeholder="phoneNumber" value={phonenumer}  onChange={(e)=>{setphonenumer(e.target.value)
+                    }}/>
+                    <input type="text" placeholder="customername" value={customername}  onChange={(e)=>{setcustomername(e.target.value)
+                    }}/>
+                    <button onClick={handletask}>checkin</button>
+                    </div>
+                    <button onClick={()=>{setmodalisopen(false)}}>close</button>
+           </Modal>
+            
+           {/* <div class="task-input">
+
                 <input type="text" placeholder="vehicleNumber" value={vehiclenum}  onChange={(e)=>{setvehiclenum(e.target.value)
                 }}/>
                   <input type="text" placeholder="vehicleType" value={vehicletype}  onChange={(e)=>{setvehicletype(e.target.value)
@@ -64,9 +108,9 @@ const Header = () => {
                 }}/>
                  <input type="text" placeholder="customername" value={customername}  onChange={(e)=>{setcustomername(e.target.value)
                 }}/>
-                <button onClick={handletask}>checkin</button>
-           </div>
-           <div class="task-display">
+               <button onClick={handletask}>checkin</button>
+           </div> */}
+             <div class="task-display">
                 {
                    arr.map((taskObj)=>{
                     return <div className="task-div" key={taskObj.newobj.id}>
@@ -75,7 +119,7 @@ const Header = () => {
                         {taskObj.newobj.vehiclemodel}
                         {taskObj.newobj.phonenumer}
                         {taskObj.newobj.customername}
-                       
+                        <button onClick={handlecheckout}  >checkout</button>
                         </div>;
                    })
                 }
